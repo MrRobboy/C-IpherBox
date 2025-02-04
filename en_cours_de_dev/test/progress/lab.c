cdcadb2694b72fb6a7f73a86b5766a27bf213d32
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2025-02-04 19:27:03 +0100

    qfsq

--- en_cours_de_dev/test/progress/lab.c
+++ en_cours_de_dev/test/progress/lab.c
@@ -4,6 +4,14 @@
 
 #define Max_Val_ASCII 256
 
+typedef struct Noeud Noeud;
+struct Noeud
+{
+	char caractere;
+	int frequence;
+	struct Noeud *gauche, *droite;
+};
+
 int *FillTabHzChar(char *filename, int *frequence)
 {
 	FILE *fichier = NULL;
@@ -31,6 +39,70 @@ void ShowHz(int *frequence)
 		if (frequence[i] > 0)
 			printf("Caractère '%c' (%u) : %d fois\n", i, i, frequence[i]);
 	}
+}
+
+Noeud *creerNoeud(char caractere, int frequence, Noeud *gauche, Noeud *droite)
+{
+	Noeud *noeud = (Noeud *)malloc(sizeof(Noeud));
+	if (!noeud)
+	{
+		perror("Erreur d'allocation mémoire");
+		exit(EXIT_FAILURE);
+	}
+	noeud->caractere = caractere;
+	noeud->frequence = frequence;
+	noeud->gauche = gauche;
+	noeud->droite = droite;
+	return noeud;
+}
+
+typedef struct Arbre Arbre;
+struct Arbre
+{
+	int taille;
+	int capacite;
+	Noeud **tableau;
+};
+
+Arbre *creerArbre(int capacite)
+{
+	Arbre *heap = (Arbre *)malloc(sizeof(Arbre));
+	if (!heap)
+	{
+		perror("Erreur d'allocation");
+		exit(EXIT_FAILURE);
+	}
+	heap->taille = 0;
+	heap->capacite = capacite;
+	heap->tableau = (Noeud **)malloc(capacite * sizeof(Noeud *));
+	return heap;
+}
+
+Noeud *construireArbreHuffman(int frequence[])
+{
+	Arbre *heap = creerArbre(256);
+
+	for (int i = 0; i < 256; i++)
+	{
+		if (frequence[i] > 0)
+		{
+			heap->tableau[heap->taille++] = creerNoeud(i, frequence[i], NULL, NULL);
+		}
+	}
+
+	while (heap->taille > 1)
+	{
+		Noeud *gauche = extraireMin(heap);
+		Noeud *droite = extraireMin(heap);
+
+		// Création du nœud interne fusionné
+		Noeud *nouveauNoeud = creerNoeud('\0', gauche->frequence + droite->frequence, gauche, droite);
+
+		// Insertion du nœud fusionné
+		insererArbre(heap, nouveauNoeud);
+	}
+
+	return extraireMin(heap); // La racine de l’arbre
 }
 
 int main()
---
+++ en_cours_de_dev/test/progress/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	int i = 150;
+	printf("first : %d\n", i);	// first :
+	printf("second : %d\n", i + 1); // second :
+	printf("thrid : %d\n", i++);	// third :
+	printf("fourth : %d\n", i);	// fourth :
+}
