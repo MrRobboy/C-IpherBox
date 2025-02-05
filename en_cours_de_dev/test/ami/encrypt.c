008fbb15a6c335cde15138e95643e822e1af1c6d
MrRobboy <mathis.ngoo@gmail.com>
2025-02-05 18:19:21 +0100

    some gits

--- en_cours_de_dev/test/ami/encrypt.c
+++
@@ -1,7 +0,0 @@
-void encryptDecrypt(char *data, int dataLen, const char *key, int keyLen)
-{
-	for (int i = 0; i < dataLen; i++)
-	{
-		data[i] ^= key[i % keyLen]; // XOR pour chiffrer/déchiffrer
-	}
-}
